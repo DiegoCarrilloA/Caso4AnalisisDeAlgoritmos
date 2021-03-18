a83e8978fcf5046fcc7ff0c939492d0105981bdb
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2021-03-17 20:00:56 -0600

    Update PuntosCaso4.cpp

--- PuntosCaso4.cpp
+++ PuntosCaso4.cpp
@@ -12,6 +12,84 @@
 #include <iostream>
 using namespace std;
 
+void makePattern(int pWidth, int pHeight){
+	//Toma las dimensiones proporcionadas y las reduce  en un tercio para tener un espacio de trabajo más pequeño;
+	int tirdWidth, tirdHeight;
+	tirdWidth = pWidth/3;
+	tirdHeight = pHeight/3;
+
+	//Se reducen otra vez estos nuevo tamaños
+	int halfTirdWi, halfTirdHe;
+	halfTirdWi = tirdWidth/2;
+	halfTirdHe = tirdHeight/2;
+
+/*
+
+El diseño se basa en 7 puntos básicos
+Que serán la base para todos los demás
+								_____________________________________
+								|	  Tamaño total = halfTirdWi;	|
+								|___________________________________|
+
+
+							   		↓ Punto B = (halfTirdHe/6,0)
+				 				 ___._______________________________											___
+								|									' Punto C = (halfTirdWi, 0)					|
+ Punto A = (0, halfTirdWi/6) -->.									|											|
+								|									|											|
+								|									|											|
+								|Punto D =((halfTirdWi/3)*2, 		|											| Tamaño total = halfTirdHe
+								| ((halfTirdHe/3)*2) |				|											|
+								|					 ↓				. <- Punto E = (halfTirdWi,(halfTirdHe/2));	|
+								|  					 .				|											|
+								|									|											|
+								|									|											|
+								|									|											|
+								|									|											|
+	 Punto F = (0, halfTirdHe)->.__________________.________________|											|____
+
+												   ↑ Punto G = (halfTirdWi/2, halfTirdHe);
+
+
+Con estos puntos podemos realizar 4 rectas que serán la base para el diseño.
+
+Recta 1 = A-E;
+Recta 2 = B-G;
+Recta 3 = F-D;
+Recta 4 = D-C;
+*/
+
+	int matriz[4][4] = {};
+	//Recta 1
+	matriz[0][0]= 0;
+	matriz[0][1]= (int) (halfTirdWi/6);
+	matriz[0][2]= halfTirdWi;
+	matriz[0][3]=(int) (halfTirdHe/2);
+
+	//Recta 2
+	matriz[1][0] = (int)(halfTirdHe/6);
+	matriz[1][1] = 0;
+	matriz[1][2] = (int)(halfTirdWi/2);
+	matriz[1][3] = halfTirdHe;
+
+	//Recta 3
+	matriz[2][0] = 0;
+	matriz[2][1] = halfTirdHe ;
+	matriz[2][2] = (int)((halfTirdWi/3)*2);
+	matriz[2][3] = (int)((halfTirdHe/3)*2);
+
+	//Recta 4
+	matriz[3][0] = (int)((halfTirdWi/3)*2);
+	matriz[3][1] = (int)((halfTirdHe/3)*2);
+	matriz[3][2] = halfTirdWi;
+	matriz[3][3] = 0;
+	for(int i= 0; i<4; i++){
+		printf("g.drawLine(%d,%d,%d,%d);\n", matriz[i][0],matriz[i][1],matriz[i][2],matriz[i][3]);
+	}
+
+}
+
+
 
 void encuentraPuntos(int ancho, int largo){
     
