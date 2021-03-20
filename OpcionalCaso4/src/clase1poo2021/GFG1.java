8ffd1166fe7caf9809c658ab94b551bfc9c35054
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2021-03-19 19:35:44 -0600

    Add files via upload

---
+++ OpcionalCaso4/src/clase1poo2021/GFG1.java
@@ -0,0 +1,196 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1poo2021;
+import static clase1poo2021.MyCanvas.X;
+import static clase1poo2021.MyCanvas.Y;
+import java.awt.*;
+import javax.swing.*;
+
+class MyCanvas extends JComponent { 
+       
+    //Indicar tamaño de la ventana
+    static final int X = 1920;
+    static final int Y = 1080;
+    
+    @Override
+    public void paint(Graphics g) 
+    { 
+        
+        
+        
+        // Pegar la salida del programa en C++
+        g.drawLine(0,53,320,90);
+        g.drawLine(30,0,160,180);
+        g.drawLine(0,180,212,120);
+        g.drawLine(212,120,320,0);
+        g.drawLine(640,53,320,90);
+        g.drawLine(610,0,480,180);
+        g.drawLine(640,180,428,120);
+        g.drawLine(428,120,320,0);
+        g.drawLine(640,53,960,90);
+        g.drawLine(670,0,800,180);
+        g.drawLine(640,180,852,120);
+        g.drawLine(852,120,960,0);
+        g.drawLine(1280,53,960,90);
+        g.drawLine(1250,0,1120,180);
+        g.drawLine(1280,180,1068,120);
+        g.drawLine(1068,120,960,0);
+        g.drawLine(1280,53,1600,90);
+        g.drawLine(1310,0,1440,180);
+        g.drawLine(1280,180,1492,120);
+        g.drawLine(1492,120,1600,0);
+        g.drawLine(1920,53,1600,90);
+        g.drawLine(1890,0,1760,180);
+        g.drawLine(1920,180,1708,120);
+        g.drawLine(1708,120,1600,0);
+        g.drawLine(0,307,320,270);
+        g.drawLine(30,360,160,180);
+        g.drawLine(0,180,212,240);
+        g.drawLine(212,240,320,360);
+        g.drawLine(640,307,320,270);
+        g.drawLine(610,360,480,180);
+        g.drawLine(640,180,428,240);
+        g.drawLine(428,240,320,360);
+        g.drawLine(640,307,960,270);
+        g.drawLine(670,360,800,180);
+        g.drawLine(640,180,852,240);
+        g.drawLine(852,240,960,360);
+        g.drawLine(1280,307,960,270);
+        g.drawLine(1250,360,1120,180);
+        g.drawLine(1280,180,1068,240);
+        g.drawLine(1068,240,960,360);
+        g.drawLine(1280,307,1600,270);
+        g.drawLine(1310,360,1440,180);
+        g.drawLine(1280,180,1492,240);
+        g.drawLine(1492,240,1600,360);
+        g.drawLine(1920,307,1600,270);
+        g.drawLine(1890,360,1760,180);
+        g.drawLine(1920,180,1708,240);
+        g.drawLine(1708,240,1600,360);
+        g.drawLine(0,413,320,450);
+        g.drawLine(30,360,160,540);
+        g.drawLine(0,540,212,480);
+        g.drawLine(212,480,320,360);
+        g.drawLine(640,413,320,450);
+        g.drawLine(610,360,480,540);
+        g.drawLine(640,540,428,480);
+        g.drawLine(428,480,320,360);
+        g.drawLine(640,413,960,450);
+        g.drawLine(670,360,800,540);
+        g.drawLine(640,540,852,480);
+        g.drawLine(852,480,960,360);
+        g.drawLine(1280,413,960,450);
+        g.drawLine(1250,360,1120,540);
+        g.drawLine(1280,540,1068,480);
+        g.drawLine(1068,480,960,360);
+        g.drawLine(1280,413,1600,450);
+        g.drawLine(1310,360,1440,540);
+        g.drawLine(1280,540,1492,480);
+        g.drawLine(1492,480,1600,360);
+        g.drawLine(1920,413,1600,450);
+        g.drawLine(1890,360,1760,540);
+        g.drawLine(1920,540,1708,480);
+        g.drawLine(1708,480,1600,360);
+        g.drawLine(0,667,320,630);
+        g.drawLine(30,720,160,540);
+        g.drawLine(0,540,212,600);
+        g.drawLine(212,600,320,720);
+        g.drawLine(640,667,320,630);
+        g.drawLine(610,720,480,540);
+        g.drawLine(640,540,428,600);
+        g.drawLine(428,600,320,720);
+        g.drawLine(640,667,960,630);
+        g.drawLine(670,720,800,540);
+        g.drawLine(640,540,852,600);
+        g.drawLine(852,600,960,720);
+        g.drawLine(1280,667,960,630);
+        g.drawLine(1250,720,1120,540);
+        g.drawLine(1280,540,1068,600);
+        g.drawLine(1068,600,960,720);
+        g.drawLine(1280,667,1600,630);
+        g.drawLine(1310,720,1440,540);
+        g.drawLine(1280,540,1492,600);
+        g.drawLine(1492,600,1600,720);
+        g.drawLine(1920,667,1600,630);
+        g.drawLine(1890,720,1760,540);
+        g.drawLine(1920,540,1708,600);
+        g.drawLine(1708,600,1600,720);
+        g.drawLine(0,773,320,810);
+        g.drawLine(30,720,160,900);
+        g.drawLine(0,900,212,840);
+        g.drawLine(212,840,320,720);
+        g.drawLine(640,773,320,810);
+        g.drawLine(610,720,480,900);
+        g.drawLine(640,900,428,840);
+        g.drawLine(428,840,320,720);
+        g.drawLine(640,773,960,810);
+        g.drawLine(670,720,800,900);
+        g.drawLine(640,900,852,840);
+        g.drawLine(852,840,960,720);
+        g.drawLine(1280,773,960,810);
+        g.drawLine(1250,720,1120,900);
+        g.drawLine(1280,900,1068,840);
+        g.drawLine(1068,840,960,720);
+        g.drawLine(1280,773,1600,810);
+        g.drawLine(1310,720,1440,900);
+        g.drawLine(1280,900,1492,840);
+        g.drawLine(1492,840,1600,720);
+        g.drawLine(1920,773,1600,810);
+        g.drawLine(1890,720,1760,900);
+        g.drawLine(1920,900,1708,840);
+        g.drawLine(1708,840,1600,720);
+        g.drawLine(0,1027,320,990);
+        g.drawLine(30,1080,160,900);
+        g.drawLine(0,900,212,960);
+        g.drawLine(212,960,320,1080);
+        g.drawLine(640,1027,320,990);
+        g.drawLine(610,1080,480,900);
+        g.drawLine(640,900,428,960);
+        g.drawLine(428,960,320,1080);
+        g.drawLine(640,1027,960,990);
+        g.drawLine(670,1080,800,900);
+        g.drawLine(640,900,852,960);
+        g.drawLine(852,960,960,1080);
+        g.drawLine(1280,1027,960,990);
+        g.drawLine(1250,1080,1120,900);
+        g.drawLine(1280,900,1068,960);
+        g.drawLine(1068,960,960,1080);
+        g.drawLine(1280,1027,1600,990);
+        g.drawLine(1310,1080,1440,900);
+        g.drawLine(1280,900,1492,960);
+        g.drawLine(1492,960,1600,1080);
+        g.drawLine(1920,1027,1600,990);
+        g.drawLine(1890,1080,1760,900);
+        g.drawLine(1920,900,1708,960);
+        g.drawLine(1708,960,1600,1080);
+     //Encierra en un rectangulo indicando las dimensiones
+    g.drawLine(0, Y, X, Y);
+    g.drawLine(X, 0, X, Y);
+   
+    } 
+} 
+  
+public class GFG1 { 
+    public static void main(String[] a) 
+    { 
+  
+        // creating object of JFrame(Window popup) 
+        JFrame window = new JFrame(); 
+        window.setBackground(Color.white);
+        // setting closing operation 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+  
+        // setting size of the pop window.setBackground(Color.white);window 
+        window.setSize(X,Y);
+  
+        // setting canvas for draw 
+        window.getContentPane().add(new MyCanvas()); 
+        window.setLocationRelativeTo(null);
+        // set visibility 
+        window.setVisible(true);
+        
+    } 
+} 
